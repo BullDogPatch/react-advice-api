08b9af79124e4260671e8a2648fd000ccb81a4e1
Craig Clayton <55770352+BullDogPatch@users.noreply.github.com>
2020-06-26 22:40:02 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import axios from "axios";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    advice: ""
+  };
+
+  componentDidMount() {
+    // console.log('componentDidMount');
+
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then(response => {
+        const { advice } = response.data.slip;
+        this.setState({ advice });
+      })
+      .catch(error => {
+        console.log(error, "NOT FOUND");
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button onClick={this.fetchAdvice} className="button">
+            <span>Get My Advice</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
